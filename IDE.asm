5e20a5234a85cf4447ff9a42d52a1d04cf1ff0f2
NoamOfficial <noamrosenblit@gmail.com>
2026-03-04 08:54:32 +0200

    Refactor IDE 48-bit read/write handling

--- IDE.asm
+++ IDE.asm
@@ -1,7 +1,14 @@
 use32
 
-; ------------------------------
-; IDE Ports
+; --------------------------
+section '.bss' align 4
+ecx_total   resd 1
+esi_chunk   resd 1
+buffer      resb 512*256      ; buffer for max 256 sectors
+
+; --------------------------
+section '.text' code readable executable
+
 IDE_DATA        equ 0x1F0
 IDE_SECCOUNT    equ 0x1F2
 IDE_LBA_LOW     equ 0x1F3
@@ -10,7 +17,6 @@ IDE_LBA_HIGH    equ 0x1F5
 IDE_DRIVE       equ 0x1F6
 IDE_STATUS      equ 0x1F7
 IDE_COMMAND     equ 0x1F7
-IDE_CONTROL     equ 0x3F6
 
 BSY equ 0x80
 DRQ equ 0x08
@@ -18,16 +24,7 @@ DRQ equ 0x08
 CMD_READ_STREAM_EXT_48   equ 0x24
 CMD_WRITE_STREAM_EXT_48  equ 0x34
 
-; ------------------------------
-section .bss align 4
-ecx_total   resd 1      ; total sectors remaining
-esi_chunk   resd 1      ; chunk size per command
-
-; ------------------------------
-section '.text' code readable executable
-
-; ------------------------------
-; wait until drive ready and DRQ set
+; ---------------------------------
 wait_ready:
     in al, IDE_STATUS
     test al, BSY
@@ -37,58 +34,56 @@ wait_ready:
     jz wait_ready
     ret
 
-; ------------------------------
-; 48-bit IDE read/write
+; ---------------------------------
+; Interrupt handler: int 0x90 for IDE
 ; AH = 0 -> write, 1 -> read
 ; EBX:EDX = starting LBA (48-bit)
-; ECX = total sectors to transfer
+; ECX = sector count
 ; ESI = buffer pointer
-ide_48bit_safe:
+; Return: EAX=0 success, 1 error
+ide_int_handler:
+    pushad                     ; save all general registers
     push ebp
     mov ebp, esp
 
-    mov [ecx_total], ecx        ; save total sectors
+    ; save total sectors
+    mov [ecx_total], ecx
 
 .next_chunk:
-    ; calculate chunk size = min(256, total remaining)
+    ; calculate chunk size = min(256, remaining)
     mov eax, [ecx_total]
     cmp eax, 256
     jle .set_chunk
     mov eax, 256
 .set_chunk:
-    mov [esi_chunk], eax        ; save chunk size
+    mov [esi_chunk], eax
 
-    ; ------------------------------
-    ; send 48-bit sector count (2 bytes)
-    mov al, ah                  ; high byte of chunk
+    ; sector count 48-bit
+    mov al, ah
     out IDE_SECCOUNT+1, al
-    mov al, al                  ; low byte
+    mov al, al
     out IDE_SECCOUNT, al
 
-    ; ------------------------------
-    ; send 48-bit LBA high bytes first
-    mov al, dl                  ; bits 32-39
+    ; LBA high bytes first
+    mov al, dl
     out IDE_LBA_LOW+1, al
-    mov al, dh                  ; bits 40-47
+    mov al, dh
     out IDE_LBA_MID+1, al
     mov al, 0
-    out IDE_LBA_HIGH+1, al      ; high 16 bits (usually 0)
+    out IDE_LBA_HIGH+1, al
 
-    ; ------------------------------
-    ; send 48-bit LBA low bytes second
-    mov al, bl                  ; bits 0-7
+    ; LBA low bytes second
+    mov al, bl
     out IDE_LBA_LOW, al
-    mov al, bh                  ; bits 8-15
+    mov al, bh
     out IDE_LBA_MID, al
     mov al, 0
-    out IDE_LBA_HIGH, al        ; bits 16-23
+    out IDE_LBA_HIGH, al
 
-    ; ------------------------------
-    ; select drive + LBA mode
+    ; select drive
     mov al, 0x40
     out IDE_DRIVE, al
 
-    ; ------------------------------
     ; send command
     cmp ah, 0
     je .write_cmd
@@ -99,13 +94,12 @@ ide_48bit_safe:
 .send_cmd:
     out IDE_COMMAND, al
 
-    ; ------------------------------
-    ; data transfer per sector
+    ; transfer data per sector
     mov edi, esi
-    mov ecx, [esi_chunk]        ; sectors in this chunk
+    mov ecx, [esi_chunk]
 .next_sector:
     call wait_ready
-    mov edx, 256                ; 256 words per sector
+    mov edx, 256
 .next_word:
     cmp ah, 1
     je .do_read
@@ -123,24 +117,28 @@ ide_48bit_safe:
     dec ecx
     jnz .next_sector
 
-    ; ------------------------------
     ; advance buffer
     mov eax, [esi_chunk]
-    shl eax, 9                  ; multiply by 512 bytes
+    shl eax, 9
     add esi, eax
 
-    ; ------------------------------
     ; advance LBA
-    mov eax, [esi_chunk]
-    add ebx, eax
-    adc edx, 0                  ; handle carry into high 16 bits
+    add ebx, [esi_chunk]
+    adc edx, 0
 
-    ; ------------------------------
     ; subtract chunk from total
-    sub [ecx_total], [esi_chunk]
+    mov eax, [ecx_total]
+    sub eax, [esi_chunk]
+    mov [ecx_total], eax
+
     cmp dword [ecx_total], 0
     jne .next_chunk
 
+    ; success
+    xor eax, eax
+
     pop ebp
-    ret
+    popad
+    iret
+
     
